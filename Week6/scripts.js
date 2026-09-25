28db251ca92b8aefbb97c9e3cb8d16bb40460291
anthonys5s <anthonymsilva27@gmail.com>
2026-09-24 20:44:18 -0600

    done

---
+++ Week6/scripts.js
@@ -0,0 +1,26 @@
+let score = Number(prompt("Enter your score (0–100):"))
+if (score >= 90)
+  console.log("Grade: A")
+  else if (score >= 80) 
+  console.log("Grade: B")
+  else if (score >= 70) 
+  console.log("Grade: C")
+  else if (score >= 60) 
+  console.log("Grade: D") 
+  else 
+  console.log("Grade: F")
+
+let age = Number(prompt("Enter your age:"))
+if (age >= 18) 
+  console.log("Adult")
+  else 
+  console.log("Minor")
+
+let movieRating = Number(prompt("Rate the movie Scream 7 from 1 to 10:"))
+if (movieRating >= 8)
+  console.log("Must watch") 
+  else if (movieRating >= 8) 
+  console.log("Worth a watch") 
+  else 
+  console.log("Skip it")
+//Horrible movie btw
